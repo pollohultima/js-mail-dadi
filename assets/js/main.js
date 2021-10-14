c5d1f1ac6762edc80f34870aaf37f46bedc87c85
pollohultima <matteo.polonio@gmail.com>
2021-10-14 14:23:57 +0200

    Email completed commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,12 @@ Mail
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 */
 
-const askMail = prompt('Inserisci la tua email')
-const mailList = ['matteo.polonio@gmail.com', 'cicciopasticcio@gmail.com', 'asdrubalefinecchi@gmail.com']
+const askMail = prompt('Inserisci la tua email');
+const mailList = ['matteo.polonio@gmail.com', 'cicciopasticcio@gmail.com', 'asdrubalefinecchi@gmail.com'];
 
-console.log(askMail);
-console.log(mailList);
 
-if (askMail == mailList[0]) {
-    console.log('Email verificata!');
+if (mailList.indexOf(askMail) >= 0) {
+    alert('Email verificata! Accesso consentito');
 } else {
-    console.log('spiacente ma la tua email non è presente');
+    alert('Spiacente ma la tua email non è valida');
 }
